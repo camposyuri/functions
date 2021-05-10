b7235566d96750444ff270f91eb8ad3ce7d30288
Yuri Campos <45168468+camposyuri@users.noreply.github.com>
2021-05-10 19:54:24 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,22 +3,31 @@ const arrayHours = [1, 6, 6, 22, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 const arrayMinutes = [7, 16, 13, 13, 46, 43, 14, 29, 9, 4, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 const arraySeconds = [53, 32, 17, 55, 13, 12, 2, 3, 49, 48, 42, 41, 33, 4, 7, 52, 5, 36, 43, 23, 49, 32, 28, 50, 25, 28];
 
-const convertDaysInHours = arrayDay.map((hours) => hours * 24);
+const sizeArray = (acumulador, item) => acumulador + item;
+const twentyFour = (item) => item * 24;
+const sixty = (item) => item * 60;
+
+// Passando uma função como parâmetro para outra função
+const convertDaysInHours = arrayDay.map(twentyFour);
 console.log(convertDaysInHours); // Hours
 
-const convertHoursInMinutes = arrayHours.map((minutes) => minutes * 60);
+// Passando uma função como parâmetro para outra função
+const convertHoursInMinutes = arrayHours.map(sixty);
 console.log(convertHoursInMinutes); // Minutes
 
-const convertMinutesInSeconds = arrayMinutes.map((seconds) => seconds * 60);
+// Passando uma função como parâmetro para outra função
+const convertMinutesInSeconds = arrayMinutes.map(sixty);
 console.log(convertMinutesInSeconds); // Seconds
 
-const seconds = arraySeconds.reduce((acumulador, item) => acumulador + item);
+// Passando uma função como parâmetro para outra função
+const seconds = arraySeconds.reduce(sizeArray);
 console.log(seconds);
 
 const allArraySeconds = convertDaysInHours.concat(convertHoursInMinutes, convertMinutesInSeconds);
 console.log(allArraySeconds); // All Array together
 
-const totalInSeconds = allArraySeconds.reduce((acumulador, item) => acumulador + item);
+// Passando uma função como parâmetro para outra função
+const totalInSeconds = allArraySeconds.reduce(sizeArray);
 console.log(totalInSeconds); // Total in seconds
 
 const sumTotal = (x) => (y) => x + y;
