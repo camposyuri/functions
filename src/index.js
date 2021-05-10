132242fb07c494748e0dc093e827f7f5e88cba99
camposyuri <yurinapoleao@gmail.com>
2021-05-10 20:30:38 -0300

    Builder exercises

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const quantifyPerLiter = (km) => (liter) => km / liter;
+
+const spentPerKm = (value) => (km) => value / km;
+
+const r1 = quantifyPerLiter(500)(30);
+console.log(r1.toFixed(2));
+
+const r2 = spentPerKm(5.39)(14);
+console.log(r2.toFixed(2));
